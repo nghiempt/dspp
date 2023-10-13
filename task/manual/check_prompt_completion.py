39eee965bf1457bf53fb7152b35cd9162277b005
Hải Bằng Ngô <bangnhcce160624@fpt.edu.vn>
2023-10-13 13:30:27 +0700

    100 app manual

--- task/manual/check_prompt_completion.py
+++ task/manual/check_prompt_completion.py
@@ -1,6 +1,6 @@
 import json
 
-json_file_path = '/Users/nghiempt/Corporation/scientific_research/paper_policy/task/bangnh_bard/assets/dataset_random_bard.json'
+json_file_path = 'task\manual\dataset_manual_100.json'
 
 
 with open(json_file_path, 'r') as file:
@@ -9,8 +9,8 @@ with open(json_file_path, 'r') as file:
 for index, item in enumerate(data):
     print("========================== " +
           str(index) + " times ~~ " + str(item['package_name'])  + " ==========================")
-    print(item['completion'])
-    if (index > 2):
-        break
+    print(item['prompt'])
+    if index < len(data) - 1:
+        input()
 
 # >>>>> Incorrect: 1\nIncomplete: 1\nInconsistent: 1 <<<<<
