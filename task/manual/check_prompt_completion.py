b5b2ca6c36c5dc24baa8a3bfd196fe0b33275999
Hung <hung16132016@gmail.com>
2023-10-13 13:07:21 +0700

    [UPDATE] done manual 200 csv

--- task/manual/check_prompt_completion.py
+++ task/manual/check_prompt_completion.py
@@ -1,16 +1,16 @@
 import json
 
-json_file_path = '/Users/nghiempt/Corporation/scientific_research/paper_policy/task/bangnh_bard/assets/dataset_random_bard.json'
+json_file_path = '/Users/nguyennhathung/dspp/task/manual/dataset_manual_200.json'
 
 
 with open(json_file_path, 'r') as file:
     data = json.load(file)
 
 for index, item in enumerate(data):
-    print("========================== " +
+    print("\n========================== " +
           str(index) + " times ~~ " + str(item['package_name'])  + " ==========================")
-    print(item['completion'])
-    if (index > 2):
-        break
+    print(item['prompt'])
+    if index < len(data) - 1:
+        input()
 
 # >>>>> Incorrect: 1\nIncomplete: 1\nInconsistent: 1 <<<<<
