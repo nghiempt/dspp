0b1e70ac89e2b1de5c023c59e23b7da19abe55cf
Nghiem Thanh Pham <nghiempt.dev@gmail.com>
2023-10-14 11:37:03 +0700

    config cm

--- _checklist/times01/confusion_matrix.py
+++ _checklist/times01/confusion_matrix.py
@@ -1,12 +1,12 @@
 import pandas as pd
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
 
 # Read the actual and predicted CSV files into dataframes
 actual_df = pd.read_csv('/Users/nghiempt/Corporation/scientific_research/paper_policy/_checklist/times01/dataset_manual_100.csv')
 predict_df = pd.read_csv('/Users/nghiempt/Corporation/scientific_research/paper_policy/_checklist/times01/origin_m100.csv')
 
-# Initialize an empty dictionary to store confusion matrices
-confusion_matrices = {}
+# Initialize an empty dictionary to store confusion matrices and metrics
+results = {}
 
 # Iterate through each column in the dataframes
 for column in actual_df.columns:
@@ -15,10 +15,25 @@ for column in actual_df.columns:
     predicted_values = predict_df[column]
     cm = confusion_matrix(actual_values, predicted_values)
     
-    # Store the confusion matrix in the dictionary
-    confusion_matrices[column] = cm
+    # Calculate accuracy, precision, and recall
+    accuracy = accuracy_score(actual_values, predicted_values)
+    precision = precision_score(actual_values, predicted_values)
+    recall = recall_score(actual_values, predicted_values)
+    
+    # Store the confusion matrix and metrics in the results dictionary
+    results[column] = {
+        'Confusion Matrix': cm,
+        'Accuracy': accuracy,
+        'Precision': precision,
+        'Recall': recall
+    }
 
-# Print the confusion matrices
-for column, cm in confusion_matrices.items():
-    print(f"Confusion Matrix for '{column}':")
-    print(cm)
+# Print the results
+for column, metrics in results.items():
+    print(f"Metrics for '{column}':")
+    print("Confusion Matrix:")
+    print(metrics['Confusion Matrix'])
+    print(f"Accuracy: {metrics['Accuracy']:.2f}")
+    print(f"Precision: {metrics['Precision']:.2f}")
+    print(f"Recall: {metrics['Recall']:.2f}")
+    print()
