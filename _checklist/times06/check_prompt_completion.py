6a0db76307201e2fec1bc2b9fb2739d8b4179150
Hải Bằng Ngô <bangnhcce160624@fpt.edu.vn>
2023-10-14 17:22:34 +0700

    times06 csv llama generate

--- _checklist/times06/check_prompt_completion.py
+++ _checklist/times06/check_prompt_completion.py
@@ -1,15 +1,16 @@
 import json
 
-json_file_path = 'm2.json'
+json_file_path = 'llama_generate.json'
 
 
-with open(json_file_path, 'r') as file:
+with open(json_file_path, 'r', encoding='utf-8') as file:
     data = json.load(file)
 
 for index, item in enumerate(data):
+    if index < 200:
+        continue
     print("========================== " +
           str(index + 2) + " times ==========================")
     print(item['completion'])
 
-    # if (index > 98):
-    #     break
+    
