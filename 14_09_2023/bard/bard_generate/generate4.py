9be6d034664d53ef8e378cfcb578adc311573496
Nghiem Thanh Pham <nghiempt.dev@gmail.com>
2023-10-14 09:36:20 +0700

    set up bard

--- 14_09_2023/bard/bard_generate/generate4.py
+++ 14_09_2023/bard/bard_generate/generate4.py
@@ -12,8 +12,8 @@ load_dotenv()
 os.environ['_BARD_API_KEY'] = os.getenv("BARD_API_KEY")
 
 # Define the file path of the JSON file to read
-input_json_file_path = ''       # Provide the path to the input JSON file
-output_json_file_path = ''      # Provide the path to the output JSON file
+input_json_file_path = '/Users/nghiempt/Corporation/scientific_research/paper_policy/14_09_2023/bard/bard_generate/input4.json'       # Provide the path to the input JSON file
+output_json_file_path = '/Users/nghiempt/Corporation/scientific_research/paper_policy/14_09_2023/bard/bard_generate/output4.json'      # Provide the path to the output JSON file
 
 # Open and read the JSON file
 with open(input_json_file_path, 'r') as file:
