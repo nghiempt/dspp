709d4404e4977bbcc4d5dc1b1f44f8c4b6b36fd5
Nghiem Thanh Pham <nghiempt.dev@gmail.com>
2023-10-14 09:13:00 +0700

    new version

--- helpers/count_pair.py
+++ helpers/count_pair.py
@@ -1,6 +1,6 @@
 import json
 
-json_file_path = '/Users/nghiempt/Corporation/scientific_research/paper_policy/task/manual/dataset_manual_200.json'
+json_file_path = '/Users/nghiempt/Corporation/scientific_research/paper_policy/task/hungnn_llama/hungnn_llama_nodejs/input.json'
 
 with open(json_file_path, 'r') as file:
     data = json.load(file)
